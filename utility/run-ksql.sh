3b1b40259af90c564afcb1b64a68f5b62e618915
Benedikt Linse <benedikt@confluent.io>
2021-02-11 15:08:12 +0000

    adding script for running ksql-cli

--- utility/run-ksql.sh
+++ utility/run-ksql.sh
@@ -2,11 +2,27 @@
 
 set -u -e 
 
+USER=benedikt
+NODE=benedikt-ansible-c-0
+ZONE=us-central1-a
+BOOTSTRAP_SERVERS=benedikt-ansible-b-0:9091,benedikt-ansible-b-1:9091,benedikt-ansible-b-2:9091
+
+echo "Copying generated truststore from ${NODE}"
+gcloud compute ssh ${USER}@${NODE} \
+  --zone ${ZONE} \
+  --command "sudo cp /var/ssl/private/kafka_connect.truststore.jks /tmp"
+gcloud compute ssh ${USER}@${NODE} \
+  --zone ${ZONE} \
+  --command "sudo chmod a+r /tmp/kafka_connect.truststore.jks"
+gcloud compute scp \
+  --zone ${ZONE} \
+  ${USER}@${NODE}:/tmp/kafka_connect.truststore.jks .
+
 export KSQL_LOG4J_OPTS="-Dlog4j.configuration=file:log4j-file.properties"
 
 ksql \
   --user superUser \
   --password superUser \
+  --config-file ksql-cli.properties \
   https://benedikt-ansible-c-0:8088
 
-# --config-file ksql-cli.properties \
