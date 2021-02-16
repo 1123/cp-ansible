ba91b33a6916a005ca93b846fc07df0aed31bd5f
Benedikt Linse <benedikt@confluent.io>
2021-02-16 11:25:49 +0000

    adding scripts for checking via kafka-console-consumer, kafka-console-producer

---
+++ checks/clients/consume-via-tls.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -u
+
+BOOTSTRAP_SERVERS=vm1:9091,vm2:9091,vm3:9091
+
+export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:./tools-log4j.properties"
+
+echo "Consuming some data"
+kafka-console-consumer \
+  --bootstrap-server ${BOOTSTRAP_SERVERS}\
+  --consumer.config client-ssl.properties \
+  --topic test \
+  --from-beginning
--- checks/clients/copy-truststore.sh
+++ checks/clients/copy-truststore.sh
@@ -17,14 +17,3 @@ gcloud compute ssh ${USER}@${NODE} \
 gcloud compute scp \
   --zone ${ZONE} \
   ${USER}@${NODE}:/tmp/kafka_connect.truststore.jks .
-
-export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:/home/benedikt/cp-ansible/scripts/clients/tools-log4j.properties"
-
-echo "Producing some data"
-while true; do
-  echo $(date)
-  sleep 1 
-done | kafka-console-producer \
-  --bootstrap-server ${BOOTSTRAP_SERVERS}\
-  --producer.config client-ssl.properties \
-  --topic test
