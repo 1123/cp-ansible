ce49622b9bac41c575be411fa4648cc2f4685427
Benedikt Linse <benedikt@confluent.io>
2021-02-17 08:44:52 +0000

    adding installation check for rest proxy

---
+++ checks/copy-truststore-and-convert-to-pem-format.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e -u
+
+USER=benedikt
+NODE=vm5
+ZONE=us-central1-a
+BOOTSTRAP_SERVERS=vm1:9091,vm2:9091,vm3:9091
+
+rm truststore.*
+
+echo "Copying generated truststore from ${NODE}"
+gcloud compute ssh ${USER}@${NODE} \
+  --zone ${ZONE} \
+  --command "sudo cp /var/ssl/private/kafka_connect.truststore.jks /tmp"
+gcloud compute ssh ${USER}@${NODE} \
+  --zone ${ZONE} \
+  --command "sudo chmod a+r /tmp/kafka_connect.truststore.jks"
+gcloud compute scp \
+  --zone ${ZONE} \
+  ${USER}@${NODE}:/tmp/kafka_connect.truststore.jks ./truststore.jks
+
+keytool \
+  -importkeystore \
+  -srckeystore truststore.jks \
+  -destkeystore truststore.p12 \
+  -srcstoretype JKS \
+  -deststoretype PKCS12 \
+  -deststorepass confluenttruststorepass \
+  -srcstorepass confluenttruststorepass \
+  -noprompt
+openssl pkcs12 \
+  -passin pass:confluenttruststorepass \
+  -in truststore.p12 \
+  -out truststore.pem
+
