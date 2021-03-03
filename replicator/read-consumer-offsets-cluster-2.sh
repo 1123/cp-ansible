a343601b58f5bf9617dcc8fa01f282c73a275a29
Benedikt Linse <benedikt@confluent.io>
2021-03-03 17:18:42 +0000

    adding scripts for reading consumer offsets from source and sink clusters

---
+++ replicator/read-consumer-offsets-cluster-2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e -u
+
+BOOTSTRAP_SERVERS=vm4:9091,vm5:9091,vm6:9091
+
+export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:./tools-log4j.properties"
+
+kafka-console-consumer \
+  --bootstrap-server ${BOOTSTRAP_SERVERS} \
+  --consumer.config consumer-timestamps-consumer.properties \
+  --topic '__consumer_offsets' \
+  --property print.key=true \
+  --property key.deserializer=io.confluent.connect.replicator.offsets.GroupTopicPartitionDeserializer \
+  --property value.deserializer=io.confluent.connect.replicator.offsets.TimestampAndDeltaDeserializer \
+  --formatter "kafka.coordinator.group.GroupMetadataManager\$OffsetsMessageFormatter" \
+  --from-beginning | \
+grep -v confluent-audit-log-events | grep -v controlcenter
+
+
