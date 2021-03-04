12b0ddbf094cf94d0c969a7d5939a311af2431f5
Benedikt Linse <benedikt@confluent.io>
2021-03-04 12:28:08 +0000

    adding test for replicator with schema registry in master slave mode

---
+++ replicator/avro-client-test/consume-from-cluster-2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -u
+
+BOOTSTRAP_SERVERS=vm4:9091,vm5:9091,vm6:9091
+
+export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:./tools-log4j.properties"
+
+echo "Consuming order data in avro format"
+kafka-avro-console-consumer \
+  --bootstrap-server ${BOOTSTRAP_SERVERS}\
+  --consumer-property security.protocol=SASL_SSL \
+  --consumer-property sasl.mechanism=SCRAM-SHA-512 \
+  --consumer-property sasl.jaas.config='org.apache.kafka.common.security.scram.ScramLoginModule required username="admin" password="admin-secret";' \
+  --consumer-property ssl.truststore.location=truststore.jks \
+  --consumer-property ssl.truststore.password=confluenttruststorepass \
+  --property schema.registry.url=https://vm6:8081 \
+  --property schema.registry.ssl.truststore.location=truststore.jks \
+  --property schema.registry.ssl.truststore.password=confluenttruststorepass \
+  --property basic.auth.credentials.source=USER_INFO \
+  --property basic.auth.user.info=superUser:superUser \
+  --topic test-avro \
+  --from-beginning
